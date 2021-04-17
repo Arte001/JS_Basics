238c099a9a995dbf2fe7d0ac527e144ffc8af910
Arte001 <mironenkoartyom93@gmail.com>
2021-04-17 22:45:47 +0700

    1

--- 05_ifelse/js/script.js
+++ 05_ifelse/js/script.js
@@ -1,9 +1,13 @@
 let number = prompt ("enter the number");
 number = parseInt(number);
 
-if(number==0) {
-	console.log(-number);
-}
-else if(number>0) {
+if(number<0) {
 	console.log(number);
 }
+else if(number==0) {
+	console.log(0);
+}
+else {
+	console.log(number);
+}
+
