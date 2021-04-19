fd23efbaa25caa2db9c307f6f6a0a0d13e933dd0
Arte001 <mironenkoartyom93@gmail.com>
2021-04-19 17:43:58 +0700

    1

--- 07_RSP/js/script.js
+++ 07_RSP/js/script.js
@@ -8,9 +8,9 @@ let player = parseInt(prompt('Your choice: 0 - rock, 1 - scissors, 2 - paper'));
 let computer = Math.floor(Math.random() * 3);
 
 // опишем все условия и будем выводить в консоль 'computer win' или 'player win'
-if ((player==0 && computer==1) || (player==1 && computer==2) || (player==2 && computer==0)){ 
+if ((player==rock && computer==scissors) || (player==scissors && computer==paper) || (player==paper && computer==rock)){ 
  console.log('player win');
-} else if ((player==0 && computer==2) || (player==1 && computer==0) || (player==2 && computer==1)){
+} else if ((player==rock && computer==paper) || (player==scissors && computer==rock) || (player==paper && computer==scissors)){
 	console.log('computer win');
 } else {
 	console.log('draw');
