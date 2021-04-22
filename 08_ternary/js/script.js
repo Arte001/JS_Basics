f75ac3ebde64e991e59dd3a761935fadadf36216
Arte001 <mironenkoartyom93@gmail.com>
2021-04-22 23:43:38 +0700

    8

---
+++ 08_ternary/js/script.js
@@ -0,0 +1,14 @@
+let number = prompt ("enter the number");
+number = parseInt(number);
+
+/*if(number<0) {
+	console.log(-number);
+}
+else if(number==0) {
+	console.log(0);
+}
+else {
+	console.log(number);
+}
+*/
+(number >= 0) ? console.log(number) : console.log(-number);
