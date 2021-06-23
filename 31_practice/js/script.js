b3704cac15c406814993b5137d2053bf824d029e
Arte001 <mironenkoartyom93@gmail.com>
2021-06-23 17:57:18 +0700

    1

---
+++ 31_practice/js/script.js
@@ -0,0 +1,41 @@
+let crypto = [
+  {
+    name: "Bitcoin",price: 1388.37},
+  {
+    name: "Ethereum",
+    price: 716
+  },
+  {
+    name: "Litecoin",
+    price: 140
+  }
+]
+
+/*crypto.forEach(function(currency, index, cArr) {
+  console.log(currency);
+})
+
+crypto.forEach(currency => console.log(currency.name));
+
+let name = document.createElement('div');
+document.body.innerHTML = name;
+document.getElementById(name).style.backgroundColor = "#000000";
+*/
+const container = document.getElementById('items');// нашли в разметке контейнер куда надо добавлять элементы
+crypto.forEach(currency => {
+  const myElement = document.createElement('div') // для каждого элемента массива создали div
+  myElement.textContent = currency.name // положили в него название текущего элемента массива
+  const myText = document.createElement('p') // для каждого элемента массива создали paragraph
+  myText.textContent = currency.price // добавили этот созданный элемент внутрь ранее найденного контейнера
+  const myBlock = document.createElement('div') //для каждого элемента массива создали div
+  myElement.classList.add('element') // наложили class
+  myText.classList.add('text') // наложили class
+  myBlock.classList.add('block') // наложили class
+  container.append(myElement) // добавили этот созданный элемент внутрь ранее найденного контейнера
+  container.append(myText) // добавили этот созданный элемент внутрь ранее найденного контейнера
+  container.append(myBlock) // добавили этот созданный элемент внутрь ранее найденного контейнера
+  Document.getElementsByClassName('myBlock')style.width="1000px" //назначили ширину
+  Document.getElementsByClassName('myBlock')style.color="red" // назначили цвет
+});
+
+
