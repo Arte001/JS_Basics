51ba636dcb884dc8446f84f633302ea77f54d396
Arte001 <mironenkoartyom93@gmail.com>
2021-06-28 23:45:34 +0700

    1

--- 31_practice/js/script.js
+++ 31_practice/js/script.js
@@ -21,13 +21,20 @@ let name = document.createElement('div');
 document.body.innerHTML = name;
 document.getElementById(name).style.backgroundColor = "#000000";
 */
-const container = document.getElementById('items');// нашли в разметке контейнер куда надо добавлять элементы
+/*const container = document.getElementById('items');// нашли в разметке контейнер куда надо добавлять элементы
 crypto.forEach(currency => {
   const myElement = document.createElement('div') // для каждого элемента массива создали div
+  //const bitcoinContainer = document.createElement('div') // создали контейнер для BTC
+  //const ethereumContainer = document.createElement('div') // создали контейнер для ETC
+  //const litecoinContainer = document.createElement('div') // создали контейнер для LTC
   myElement.textContent = currency.name // положили в него название текущего элемента массива
   const myText = document.createElement('p') // для каждого элемента массива создали paragraph
   myText.textContent = currency.price // добавили этот созданный элемент внутрь ранее найденного контейнера
   const myBlock = document.createElement('div') //для каждого элемента массива создали div
+  const elementTitle = document.createElement('h2')
+  const bitcoinContainer = document.createElement('div')
+  bitcoinContainer.classList.add('bitcoin')
+  bitcoinContainer.append(elementTitle)
   myElement.classList.add('element') // наложили class
   myText.classList.add('text') // наложили class
   myBlock.classList.add('block') // наложили class
@@ -38,5 +45,29 @@ crypto.forEach(currency => {
   myBlock.style.height="15px" // назначили высоту
   myBlock.classList.add(currency.name.toLowerCase()) //устанавливаем css класс для каждой шкалы
 });
+*/
+const container = document.getElementById('items');
+crypto.forEach(currency => {
+  const myTitle = document.createElement('h2')
+  myTitle.textContent = currency.name
+  const myText = document.createElement('p')
+  myText.textContent = currency.price
+  const myBlock = document.createElement('div')
+  myTitle.classList.add('element') // наложили class
+  myText.classList.add('text') // наложили class
+  myBlock.classList.add('block') // наложили class
+  myBlock.style.width = currency.price / 10 + 'px'
+  myBlock.style.height="15px"
+  myBlock.classList.add(currency.name.toLowerCase())
+  const blockContainer = document.createElement('div')
+  blockContainer.classList.add(currency.name + 'Class')
+  
+  container.append(blockContainer)
+  blockContainer.append(myTitle)
+  blockContainer.append(myText)
+  blockContainer.append(myBlock)
+});
+
+
 
 
