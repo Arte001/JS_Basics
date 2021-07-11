679922c0a0c0d047cb01fb10fc0a9058c0cb2e5f
Arte001 <mironenkoartyom93@gmail.com>
2021-07-11 15:05:46 +0700

    1

--- 34_Weatherform/js/script.js
+++ 34_Weatherform/js/script.js
@@ -3,24 +3,30 @@ const APIKey = 'ed41bdd2f66cf5d745c3e52938a48f99';
 /*const city = 'Miami';*/
 
 const form = document.forms[0];
+const submit = document.forms[1];
 const insert = document.querySelector('.total');
-
 
 const url = 'http://api.openweathermap.org/data/2.5/weather?q='+form+'&appid='+APIKey;
 
 let xhr = new XMLHttpRequest();
 
-
-
 xhr.open('GET', url, false);
 
-xhr.send();
+submit.addEventListener('click', function(){
 if(xhr.status !=200) {
 	console.log(xhr.status + ' ' + xhr.statusText);
 } else {
 	let DATA = JSON.parse(xhr.responseText);
-	let celsium = DATA - 273;
-	insert.innerHTML = celsium.main.temp;
-
+	console.log(DATA);
 }
+
+
+/*xhr.send();
+		if(xhr.status !=200) {
+		console.log(xhr.status + ' ' + xhr.statusText);
+		} else {
+		let DATA = JSON.parse(xhr.responseText);
+		let celsium = DATA - 273;
+		insert.innerHTML = celsium.main.temp;
+}*/
 
