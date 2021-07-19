c2eb60b1aea9e1f7c01920b3a88232bb54a616fa
Arte001 <mironenkoartyom93@gmail.com>
2021-07-19 17:44:35 +0700

    1

--- 34_Weatherform/js/script.js
+++ 34_Weatherform/js/script.js
@@ -1,54 +1,30 @@
-/*const APIKey = 'ed41bdd2f66cf5d745c3e52938a48f99';
-
-const city = 'Miami';
-
 const form = document.forms[0];
-const submit = document.forms[1];
-const insert = document.querySelector('.total');
-
-const url = 'http://api.openweathermap.org/data/2.5/weather?q='+form+'&appid='+APIKey;
-
-let xhr = new XMLHttpRequest();
-
-xhr.open('GET', url, false);
-
-submit.addEventListener('click', function(){
-if(xhr.status !=200) {
-	console.log(xhr.status + ' ' + xhr.statusText);
-} else {
-	let DATA = JSON.parse(xhr.responseText);
-	let celsium = DATA - 273;
-	insert.innerHTML = celsium.main.temp;
-}
-*/
-
-/*xhr.send();
-		if(xhr.status !=200) {
-		console.log(xhr.status + ' ' + xhr.statusText);
-		} else {
-		let DATA = JSON.parse(xhr.responseText);
-		let celsium = DATA - 273;
-		insert.innerHTML = celsium.main.temp;
-}*/
-
-const APIKey = 'ed41bdd2f66cf5d745c3e52938a48f99';
-
-
-const form = document.forms[0];
-const insert = document.querySelector('.total');
-
-const url = 'http://api.openweathermap.org/data/2.5/weather?q='+form+'&appid='+APIKey;
-let xhr = new XMLHttpRequest();
 
 form.addEventListener('submit', function (e) {
-e.preventDefault();
-xhr.open('GET', url, false);
-xhr.send();
-let DATA = JSON.parse(xhr.responseText);
-console.log(DATA);
-if(xhr.status != 200){
-    console.log(xhr.status + ' ' + xhr.statusText);
-}else{
-    insert.innerHTML = ' ' + (DATA.main.temp - 273,5);
-}
+
+	e.preventDefault();
+
+	let city = document.forms[0].elements.weather.value;
+
+	const APIKey = 'ed41bdd2f66cf5d745c3e52938a48f99';
+
+	const url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+APIKey;
+
+	let xhr = new XMLHttpRequest();
+
+	xhr.open('GET', url, false);
+
+	xhr.send();
+	
+	let DATA = JSON.parse(xhr.responseText);
+
+	console.log(DATA);
+
+	const insert = document.querySelector('.total');
+
+	if(xhr.status != 200){
+   	 console.log(xhr.status + ' ' + xhr.statusText);
+	}else{
+    	insert.innerHTML = ' ' + (DATA.main.temp - 273,5);
+	}
 })
